0a1354671a8d963f0ec6c2f9c9aa2d415c7fad69
geovannaprates <geovannaprates@alunos.utfpr.edu.br>
2025-04-02 09:11:19 -0300

    Update and rename bla.c to comp.c

--- comp.c
+++ comp.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+#include  <stdio.h>
 int main(){
-
     int hora, min, seg;
     float aux = 5.5;
 
     printf("digite hora, minuto e segundo: ");
-    scanf("%d %d %d," &hora, &min, &seg);
+    scanf("%d %d %d", &hora, &min, &seg);
 
     // se hora maior que 1 e minuto maior que 15
-    printf("quantidade de segundos: ")
+    if (hora > 1 && min > 15) {
+    printf("quanidade de segundos: %d\n", (hora * 3600) + (min * 60) + seg);
+    } else {
     // caso contrário
+
     printf("Horario: %02d:%02d:%02d\n", hora, min, seg);
     printf("Float com duas casas: %.2f", aux);
+    }
     return 0;
 }
