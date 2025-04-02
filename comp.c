7f01f26cfb2795eb3c4d6cb6c41944473e938d76
geovannaprates <geovannaprates@alunos.utfpr.edu.br>
2025-04-02 09:42:49 -0300

    Update comp.c

--- comp.c
+++ comp.c
@@ -8,12 +8,17 @@ int main(){
 
     // se hora maior que 1 e minuto maior que 15
     if (hora > 1 && min > 15) {
-    printf("quanidade de segundos: %d\n", (hora * 3600) + (min * 60) + seg);
-    } else {
-    // caso contrário
+    float abc = ((hora * 3600) + (min * 60) + seg);
+    printf("quanidade de segundos: %f\n", abc);
+
+    }
+    else {
+         float def =((hora * 60)+ min + (seg/(60.0)));
+         printf("quantidade de minutos: %.5f\n", def);
 
     printf("Horario: %02d:%02d:%02d\n", hora, min, seg);
     printf("Float com duas casas: %.2f", aux);
     }
     return 0;
 }
+
